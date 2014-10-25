b683aacf40ce4fa0ff1c6aa7c5b1d163df16dc1d
gunnarkp13 <gunnarkp13@ru.is>
2014-10-25 18:19:47 +0000

    Bjó til byrjun á BoardTest skránni

---
+++ src/test/java/is/ru/TicTacToe/BoardTest.java
@@ -0,0 +1,16 @@
+package is.ru.TicTacToe;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class BoardTest {
+	public static void main(String args[]){
+	org.junit.runner.JUnitCore.main("is.ru.TicTacToe.BoardTest");
+	}
+
+	@Test
+	public void testConstructor(){
+	}
+
+
+}
