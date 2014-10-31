b8e2a0d983c36dcb3043a0a349de5c540ea2f4bc
einarprump <einara12@ru.is>
2014-10-31 23:38:21 +0000

    laga adeins gameweb til en ekki

--- src/selenium/java/is/ru/TicTacToe/GameWebTest.java
+++ src/selenium/java/is/ru/TicTacToe/GameWebTest.java
@@ -10,35 +10,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.*;
 import org.openqa.selenium.WebDriver;
+	
 public class GameWebTest {
-static WebDriver driver;
-static String baseUrl;
-static String port;
-@BeforeClass
-public static void before() {
-// Will be run before all tests in class are run
-driver = new FirefoxDriver();
-port = System.getenv("PORT");
-if(port == null) {
-port = "4567";
-}
-baseUrl = "http://localhost:" + port;
-driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-}
-@AfterClass
-public static void after() {
-// Will be run after all tests in class have run
-driver.close();
-}
-@Before
-public void setup() {
-// Will be run before each test in class is run
-;
-}
-@Test
-public void assertTitle() {
-driver.get(baseUrl + "/");
-assertEquals("TicTacToe", driver.getTitle());
-}
+	static WebDriver driver;
+	static String baseUrl;
+	static String port;
+	
+	@BeforeClass
+	public static void before() {
+	// Will be run before all tests in class are run
+		driver = new FirefoxDriver();
+		port = System.getenv("PORT");
+		if(port == null) {
+			port = "4567";
+		}
+		baseUrl = "http://localhost:" + port;
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@AfterClass
+	public static void after() {
+	// Will be run after all tests in class have run
+		driver.close();
+	}
 
+	@Before
+	public void setup() {
+	// Will be run before each test in class is run
+		;
+	}
+	
+	@Test
+	public void assertTitle() {
+		driver.get(baseUrl + "/");
+		assertEquals("TicTacToe", driver.getTitle());
+	}
+	
 }
+	
