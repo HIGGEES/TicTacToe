59f04bb6026fc5f7ede599130c9f05efa29ddf03
Einar Agust <einara12@ru.is>
2014-11-01 12:28:14 +0000

    laga upp eftir mig i GameWeb

--- src/main/java/is/ru/TicTacToe/GameWeb.java
+++ src/main/java/is/ru/TicTacToe/GameWeb.java
@@ -29,20 +29,39 @@ public class GameWeb implements SparkApplication {
 		post(new Route("/playTurn"){
 			@Override
 			public Object handle(Request request, Response response){
-				Integer number = Integer.valueOf(request.queryParams("playTurn"));
-				ticTacToe.playTurn(number);
-				String result;
-				if (telja == 0) {
-					telja++;
-					result = "mario";
-					return result;
-				} else {
-					telja--;
-					result = "luigo";
-					return result;
-				}
+				Integer number = Integer.valueOf(request.queryParams("playTurn")) - 1;
+					ticTacToe.playTurn(number);
+					String board = "";
+					char[] tmp = ticTacToe.getBoard();
+					for(int x = 0; x < tmp.length - 1; x++){
+						board = board + tmp[x] + ' ';
+					}
+					board = board + tmp[tmp.length - 1];
+					if(ticTacToe.gameOver()){
+						String winner = "Leik Lokid ";
+						if(ticTacToe.isWinner())
+						{
+							if(ticTacToe.getMovesLeft() % 2 == 0)
+							{
+								winner = winner + "leikmadur 1 bar sigur ur bitum";
+							}
+							else
+							{
+								winner = winner + "leikmadur 2 bar sigur ur bitum";
+							}
+						}
+						else
+						{
+							winner = winner + "enginn bar sigur ur bitum";
+						}
+						return winner;
+					}
+					else{
+						return board;
+					}
 			}
 		});
+	
 	
 		post(new Route("/newGame"){
 			@Override
