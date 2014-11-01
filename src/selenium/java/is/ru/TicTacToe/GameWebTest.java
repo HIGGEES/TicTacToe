a173eead5add4e60038bcf2c3ef73c21f6524394
Einar Agust <einara12@ru.is>
2014-11-01 01:00:46 +0000

    laga aftur

--- src/selenium/java/is/ru/TicTacToe/GameWebTest.java
+++ src/selenium/java/is/ru/TicTacToe/GameWebTest.java
@@ -27,7 +27,7 @@ public class GameWebTest {
 		if(port == null) {
 			port = "4567";
 		}
-		baseUrl = "https://ghastly-monster-9605.herokuapp.com/ "; // + port;
+		baseUrl = "https://ghastly-monster-9605.herokuapp.com "; // + port;
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
